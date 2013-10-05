9806716b58b3132187d44cbdd59b32d142fdcc01
Will Luongo <me@willluongo.com>
2013-10-05 02:27:43 -0400

    Fixes issue #2
    Prompts user to select a single channel before opening main window
    Removed prototype
    Added classpath and eclipse project to streamline builds

---
+++ src/com/willluongo/asbestos/gui/RoomSelector.java
@@ -0,0 +1,90 @@
+package com.willluongo.asbestos.gui;
+
+import java.util.ArrayList;
+
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+
+import com.madhackerdesigns.jinder.Room;
+
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Label;
+
+public class RoomSelector extends Dialog {
+
+	protected Object result;
+	protected Shell shell;
+	private ArrayList<Room> rooms;
+
+
+	/**
+	 * Create the dialog.
+	 * 
+	 * @param parent
+	 * @param style
+	 */
+	public RoomSelector(Shell parent, int style, java.util.List<Room> rooms) {
+		super(parent, style);
+		this.rooms = (ArrayList<Room>) rooms;
+		setText("Select Rooms");
+	}
+
+	/**
+	 * Open the dialog.
+	 * 
+	 * @return the result
+	 */
+	public Object open() {
+		createContents();
+		shell.open();
+		shell.layout();
+		Display display = getParent().getDisplay();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 */
+	private void createContents() {
+		shell = new Shell(getParent(), getStyle());
+		shell.setSize(450, 300);
+		shell.setText(getText());
+
+		final List list = new List(shell, SWT.BORDER);
+		list.setBounds(10, 25, 430, 225);
+		for (Room room: rooms)
+		{
+			list.add(room.name);
+		}
+
+
+		Button btnSelect = new Button(shell, SWT.NONE);
+		btnSelect.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				result = list.getSelectionIndex();
+				shell.close();
+			}
+		});
+		btnSelect.setBounds(299, 250, 94, 28);
+		btnSelect.setText("Select");
+
+		Button btnCancel = new Button(shell, SWT.NONE);
+		btnCancel.setBounds(64, 250, 94, 28);
+		btnCancel.setText("Cancel");
+		
+		Label lblSelectRooms = new Label(shell, SWT.CENTER);
+		lblSelectRooms.setBounds(187, 5, 89, 14);
+		lblSelectRooms.setText("Select Rooms");
+
+	}
+}
